03b2d8ee168ab3bdc744749ff17669cb3047abaf
BoostioA <70571116+BoostioA@users.noreply.github.com>
2020-09-01 14:06:04 +0200

    Add files via upload

---
+++ HelloWorldConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Wat is uw naam");
+
+
+
+            string naam = Console.ReadLine();
+
+            if (naam == "aaron")
+            {
+                
+
+                Console.Write("ja dat klopt " + naam);
+
+                Console.Write(Environment.NewLine);
+
+                Console.WriteLine("Wat is uw huisdier");
+
+                string huisdier = Console.ReadLine();
+                
+                if (huisdier == "yoshi") 
+
+                    Console.Write("ja dat klopt " + naam );
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
